3c5a893744925d1ab92844e0b62d63c012769a36
ybaspinar <y.baspinar@yandex.com>
2022-03-24 01:32:20 +0300

    initial release created project structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Picus-Security-Golang-Bootcamp/homework-3-week-4-ybaspinar
+
+go 1.18
---
+++ pkg/importjson/importjson.go
@@ -0,0 +1,31 @@
+package importjson
+
+import "encoding/json"
+
+type Books []Book
+
+func UnmarshalBooks(data []byte) (Books, error) {
+	var r Books
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Books) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type Book struct {
+	Bookid      int64   `json:"bookid"`
+	Booktitle   string  `json:"booktitle"`
+	Pages       int64   `json:"pages"`
+	Stockamount int64   `json:"stockamount"`
+	Price       float64 `json:"price"`
+	Stockid     int64   `json:"stockid"`
+	Isbn        int64   `json:"ısbn"`
+	Author      Author  `json:"author"`
+}
+
+type Author struct {
+	Authorid   int64  `json:"authorid"`
+	Authorname string `json:"authorname"`
+}
