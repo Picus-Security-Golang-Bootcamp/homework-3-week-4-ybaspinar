7fe220a5bc2592312e0e55bb0fae2df2b374faa8
ybaspinar <y.baspinar@yandex.com>
2022-03-25 17:07:32 +0300

    added Db connection

--- cmd/homework-3/main.go
+++ cmd/homework-3/main.go
@@ -3,18 +3,39 @@ package main
 import (
 	"fmt"
 	"github.com/Picus-Security-Golang-Bootcamp/homework-3-week-4-ybaspinar/pkg/importjson"
+	"github.com/Picus-Security-Golang-Bootcamp/homework-3-week-4-ybaspinar/pkg/newpsqldb"
+	"github.com/joho/godotenv"
 	"io/ioutil"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
 	var Books []importjson.Book
+	path, err := filepath.Abs("assets/books.json")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	//Reads JSON file
-	file, err := ioutil.ReadFile("assets/books.json")
+	file, err := ioutil.ReadFile(path)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 	Books, err = importjson.UnmarshalBooks(file)
+	println(Books[1].Bookid)
+
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := newpsqldb.NewPsqlDB()
+	if err != nil {
+		log.Fatal("Postgres cannot init:", err)
+	}
+	log.Println("Postgres connected")
 
 }
